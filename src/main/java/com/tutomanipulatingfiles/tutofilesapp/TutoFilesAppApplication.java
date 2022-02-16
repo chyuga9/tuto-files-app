644de533a8eeeb60f9c442f13f7ccaffb75c800a
Cedric <cedric.campagna@gmail.com>
2022-02-17 00:14:41 +0100

    Initialize Storage

--- src/main/java/com/tutomanipulatingfiles/tutofilesapp/TutoFilesAppApplication.java
+++ src/main/java/com/tutomanipulatingfiles/tutofilesapp/TutoFilesAppApplication.java
@@ -1,13 +1,24 @@
 package com.tutomanipulatingfiles.tutofilesapp;
 
+import javax.annotation.Resource;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tutomanipulatingfiles.tutofilesapp.service.FilesStorageService;
+
 @SpringBootApplication
-public class TutoFilesAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TutoFilesAppApplication.class, args);
-	}
-
+public class TutoFilesAppApplication implements CommandLineRunner {
+	
+  @Resource
+  FilesStorageService storageService;
+  public static void main(String[] args) {
+    SpringApplication.run(TutoFilesAppApplication.class, args);
+  }
+  
+  @Override
+  public void run(String... arg) throws Exception {
+    storageService.deleteAll();
+    storageService.init();
+  }
 }
