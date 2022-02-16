6a0b3b156064821ade73545c90ba9803fb9938a2
Cedric <cedric.campagna@gmail.com>
2022-02-16 23:43:16 +0100

    Initial files

---
+++ src/main/java/com/tutomanipulatingfiles/tutofilesapp/TutoFilesAppApplication.java
@@ -0,0 +1,13 @@
+package com.tutomanipulatingfiles.tutofilesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoFilesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoFilesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutomanipulatingfiles/tutofilesapp/TutoFilesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutomanipulatingfiles.tutofilesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoFilesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
