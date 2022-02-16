4f8d06041a75939893dbde9ace3b46fc152f0a71
Cedric <cedric.campagna@gmail.com>
2022-02-16 23:50:12 +0100

    Method load() corrected

--- src/main/java/com/tutomanipulatingfiles/tutofilesapp/service/FilesStorageService.java
+++ src/main/java/com/tutomanipulatingfiles/tutofilesapp/service/FilesStorageService.java
@@ -3,11 +3,10 @@ package com.tutomanipulatingfiles.tutofilesapp.service;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
-import javax.annotation.Resource;
-
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
-public interface FileStorageService {
+public interface FilesStorageService {
 
 	public void init();
 	  public void save(MultipartFile file);
