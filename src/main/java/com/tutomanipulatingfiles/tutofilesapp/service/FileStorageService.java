07a607abc126762351bf7088fa62432682e0f5a6
Cedric <cedric.campagna@gmail.com>
2022-02-16 23:46:07 +0100

    Methods' declaration

--- src/main/java/com/tutomanipulatingfiles/tutofilesapp/service/FileStorageService.java
+++ src/main/java/com/tutomanipulatingfiles/tutofilesapp/service/FileStorageService.java
@@ -1,5 +1,17 @@
 package com.tutomanipulatingfiles.tutofilesapp.service;
 
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public interface FileStorageService {
 
+	public void init();
+	  public void save(MultipartFile file);
+	  public Resource load(String filename);
+	  public void deleteAll();
+	  public Stream<Path> loadAll();
 }
