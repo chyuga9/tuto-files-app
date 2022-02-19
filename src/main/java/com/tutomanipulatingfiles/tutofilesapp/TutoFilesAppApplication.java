4508c2b2b349ee998ab146e58bd877223672b9ad
Cedric <cedric.campagna@gmail.com>
2022-02-19 14:56:56 +0100

    Deleting @Resource

--- src/main/java/com/tutomanipulatingfiles/tutofilesapp/TutoFilesAppApplication.java
+++ src/main/java/com/tutomanipulatingfiles/tutofilesapp/TutoFilesAppApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class TutoFilesAppApplication implements CommandLineRunner {
 	
-  @Resource
   public static void main(String[] args) {
     SpringApplication.run(TutoFilesAppApplication.class, args);
   }
