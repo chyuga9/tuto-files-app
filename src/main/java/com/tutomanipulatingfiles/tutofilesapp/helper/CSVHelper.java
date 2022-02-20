eee7fc6f9aafa5c1ced4a525022d5dd3f1562b77
Cedric <cedric.campagna@gmail.com>
2022-02-20 18:54:44 +0100

    In comments, a method to handle BOM and information about type of file to use

--- src/main/java/com/tutomanipulatingfiles/tutofilesapp/helper/CSVHelper.java
+++ src/main/java/com/tutomanipulatingfiles/tutofilesapp/helper/CSVHelper.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.ByteOrderMark;
+import org.apache.commons.io.input.BOMInputStream;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.tutomanipulatingfiles.tutofilesapp.model.Tutorial;
@@ -25,10 +28,15 @@ public class CSVHelper {
     return true;
   }
   
+
+  // https://commons.apache.org/proper/commons-csv/user-guide.html
+  // only use csv file saved as csv(delimited by semicolon) with "," as separator
+  // doesn't work with csv file saved as csv(delimited by semicolon)
   public static List<Tutorial> csvToTutorials(InputStream is) {
     try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
         CSVParser csvParser = new CSVParser(fileReader,
             CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());) {
+    	
       List<Tutorial> tutorials = new ArrayList<Tutorial>();
       Iterable<CSVRecord> csvRecords = csvParser.getRecords();
       for (CSVRecord csvRecord : csvRecords) {
@@ -45,4 +53,31 @@ public class CSVHelper {
       throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
     }
   }
+   
+  // https://commons.apache.org/proper/commons-csv/user-guide.html
+  // Handling Byte Order Marks (BOM) for csv (delimitedwith  comma OR semicolon) using coma but not semi-colon
+  // Doesn't even work with the methods withRecordSeparator() or withDelimiter()
+  /*
+  public static List<Tutorial> csvToTutorials(InputStream is) {
+	try (Reader fileReader = new InputStreamReader(new BOMInputStream(is), "UTF-8");
+        CSVParser csvParser = new CSVParser(fileReader,
+            CSVFormat.DEFAULT.withRecordSeparator(";").withDeli.withHeader());) {
+    	
+      List<Tutorial> tutorials = new ArrayList<Tutorial>();
+      Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+      for (CSVRecord csvRecord : csvRecords) {
+        Tutorial tutorial = new Tutorial(
+              Long.parseLong(csvRecord.get("Id")),
+              csvRecord.get("Title"),
+              csvRecord.get("Description"),
+              Boolean.parseBoolean(csvRecord.get("Published"))
+            );
+        tutorials.add(tutorial);
+      }
+      return tutorials;
+    } catch (IOException e) {
+      throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
+    }
+  }
+  */
 }
