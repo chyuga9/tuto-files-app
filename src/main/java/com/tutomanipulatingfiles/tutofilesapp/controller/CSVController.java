3c42a44aa6cbe8fe2f6395be1f906377416fe021
Cedric <cedric.campagna@gmail.com>
2022-02-20 18:49:18 +0100

    Added Exception's message

--- src/main/java/com/tutomanipulatingfiles/tutofilesapp/controller/CSVController.java
+++ src/main/java/com/tutomanipulatingfiles/tutofilesapp/controller/CSVController.java
@@ -34,7 +34,7 @@ public class CSVController {
         message = "Uploaded the file successfully: " + file.getOriginalFilename();
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
       } catch (Exception e) {
-        message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+        message = "Could not upload the file: " + file.getOriginalFilename() + "! "+e.getMessage();
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
       }
     }
