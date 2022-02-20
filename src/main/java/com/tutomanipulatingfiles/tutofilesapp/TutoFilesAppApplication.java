a36d4b7798131aba8ed84673097977d364dbeaa4
Cedric <cedric.campagna@gmail.com>
2022-02-20 20:39:42 +0100

    Initial classes

--- src/main/java/com/tutomanipulatingfiles/tutofilesapp/TutoFilesAppApplication.java
+++ src/main/java/com/tutomanipulatingfiles/tutofilesapp/TutoFilesAppApplication.java
@@ -5,20 +5,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.tutomanipulatingfiles.tutofilesapp.service.FilesStorageService;
 
 @SpringBootApplication
-public class TutoFilesAppApplication implements CommandLineRunner {
-	
-  @Resource
-  FilesStorageService storageService;
+public class TutoFilesAppApplication{
+
   public static void main(String[] args) {
     SpringApplication.run(TutoFilesAppApplication.class, args);
   }
-  
-  @Override
-  public void run(String... arg) throws Exception {
-    storageService.deleteAll();
-    storageService.init();
-  }
+
+
 }
